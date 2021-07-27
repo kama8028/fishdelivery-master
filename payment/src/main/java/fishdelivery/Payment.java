540cda967a8daeec7631dd89eb8760259a12f7c3
한용선 <kama8028@naver.com>
2021-07-27 11:35:28 +0900

    20차 GenerationType 변경

--- payment/src/main/java/fishdelivery/Payment.java
+++ payment/src/main/java/fishdelivery/Payment.java
@@ -17,7 +17,7 @@ import java.util.Date;
 public class Payment {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long paymentId;
     private Long orderId;
     private String customerName;
